eda3f2594b16c118c2e69c92e2096014e45dbbb9
sakuragi-zero <93202274+sakuragi-zero@users.noreply.github.com>
2022-07-26 19:09:56 +0900

    Add files via upload

--- sakuragi.py
+++ sakuragi.py
@@ -1,9 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 '''
 この関数はインデックスをカラムに変換する
-第1引数にデータフレーム名
-第2引数にリネーム後のカラム名
+
+第1引数にデータフレーム名 :obj
+第2引数にリネーム後のカラム名 :str
 data.reset_index(inplace= True)
 data = data.rename(columns={'index': 'Insight'})
+
 '''
 
 def to_index(frame, colname):
@@ -11,3 +18,20 @@ def to_index(frame, colname):
     frame = frame.rename(columns = {'index' : colname})
 
     return frame
+
+'''
+この関数はカラムを指定して要素をカウントして可視化までする
+
+第1引数にデータフレーム名 :obj
+第2引数にカウントしたいカラム名 :str
+第3引数に新しく作成するデータフレーム名 :obj
+第4引数に棒グラフのカラー :str
+
+'''
+
+def count_hist(frame, colname, new_frame, color):
+    new_frame = pd.DataFrame(frame[colname].value_counts())
+    print(colname)
+    new_frame[colname].plot.bar(
+        y = '件数', figsize = (50, 20), color = color
+    )
