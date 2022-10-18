cf34b6d02014c30868af98de5c6e0977b0bfd1d4
sakuragi-zero <93202274+sakuragi-zero@users.noreply.github.com>
2022-10-18 23:32:52 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
 from setuptools import setup
 
 setup(
-    name="Rejoui",
-    version="1.0.0",
-    install_requires=["packageA", "packageB"],
-    extras_require={
-        "develop": ["dev-packageA", "dev-packageB"]
-    },
-    entry_points={
-        "console_scripts": [
-            "foo = package_name.module_name:func_name",
-            "foo_dev = package_name.module_name:func_name [develop]"
-        ],
-        "gui_scripts": [
-            "bar = gui_package_name.gui_module_name:gui_func_name"
-        ]
-    }
-)
-
-from setuptools import setup
-
-setup(
     name = 'Rejoui',
     version = '1.0.0',
     url = 'https://github.com/sakuragi-zero/data_pipeline.git',
